051057e029580b6c1c21511bd090fa7d09139536
Wang Pengyuan <1254076181@qq.com>
2020-07-27 16:00:48 +0800

    项目第一次提交

---
+++ HotelManagementSystem014/src/main/java/com/neusoft/hotel/HotelManagementSystem014Application.java
@@ -0,0 +1,13 @@
+package com.neusoft.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystem014Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSystem014Application.class, args);
+	}
+
+}
